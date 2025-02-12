973d0929955c4f20199c721f3e68c4889488231c
Juan Peralta <juancarlosperalta0807@gmail.com>
2025-02-12 14:30:08 -0400

    .

--- Type.py
+++ Type.py
@@ -7,16 +7,45 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     word_list = ["apple", "banana", "cherry", "dog", "elephant", "frog", "grape", "horse", "iguana", "jacket",
-                 "kite", "lemon", "monkey", "nest", "orange", "penguin", "quail", "rabbit", "snake", "tiger"]
+                 "kite", "lemon", "monkey", "nest", "orange", "penguin", "quail", "rabbit", "snake", "tiger",
+                 "orange", "nurse","black", "money"]
     random.shuffle(word_list)
     current_word_index = 0
     total_words = min(15, len(word_list))
+    correct_count = 0
+    incorrect_count = 0
 
     word_display = ft.Text(value=word_list[current_word_index], size=30, weight=ft.FontWeight.BOLD)
-    page.add(word_display)
+    status_label = ft.Text(size=20)
+    input_field = ft.TextField(hint_text="Type the word", on_submit=check_word)
+
+    def check_word(e):
+        nonlocal current_word_index, correct_count, incorrect_count
+
+        user_input = input_field.value.strip()
+        if user_input == word_list[current_word_index]:
+            status_label.value = "Correct"
+            status_label.color = ft.colors.GREEN
+            correct_count += 1
+        else:
+            status_label.value = "Incorrect"
+            status_label.color = ft.colors.RED
+            incorrect_count += 1
+
+        input_field.value = ""
+        page.update()
+
+    page.add(
+        ft.Column(
+            [
+                ft.Row([word_display], alignment=ft.MainAxisAlignment.CENTER),
+                ft.Row([status_label], alignment=ft.MainAxisAlignment.CENTER),
+                ft.Row([input_field], alignment=ft.MainAxisAlignment.CENTER),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            spacing=20,
+        )
+    )
 
 ft.app(target=main)
-
-
-
 
