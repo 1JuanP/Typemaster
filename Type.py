6cca9ec4ea4fec0fb0ffe424f287de772511c6bf
Juan Peralta <juancarlosperalta0807@gmail.com>
2025-02-12 14:16:11 -0400

    .

--- Type.py
+++ Type.py
@@ -1,4 +1,22 @@
 import flet as ft
 import random
 
-#nigga
+def main(page: ft.Page):
+    page.title = "Typing Accuracy Test"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    word_list = ["apple", "banana", "cherry", "dog", "elephant", "frog", "grape", "horse", "iguana", "jacket",
+                 "kite", "lemon", "monkey", "nest", "orange", "penguin", "quail", "rabbit", "snake", "tiger"]
+    random.shuffle(word_list)
+    current_word_index = 0
+    total_words = min(15, len(word_list))
+
+    word_display = ft.Text(value=word_list[current_word_index], size=30, weight=ft.FontWeight.BOLD)
+    page.add(word_display)
+
+ft.app(target=main)
+
+
+
+
